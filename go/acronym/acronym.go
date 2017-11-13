56e67b21cb6770ec1fc127c53421e8618fb7641d
add2point71dots <alcland@gmail.com>
2017-11-12 21:24:48 -0800

    solution to acronym.go

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,5 +1,16 @@
 package acronym
 
+import "strings"
+
 const testVersion = 3
 
-func Abbreviate(string) string
+func Abbreviate(phrase string) string {
+	acronym := ""
+	for i, rune := range phrase {
+		ch := strings.ToUpper(string(rune))
+		if i == 0 || string(phrase[i-1]) == " " || string(phrase[i-1]) == "-" {
+			acronym += ch
+		}
+	}
+	return acronym
+}
